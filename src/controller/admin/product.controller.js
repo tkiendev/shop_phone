04673c81b70ed963b69854fcd2b99d136b939e95
Kien Tran <kien@example.com>
2025-09-18 15:49:13 +0700

    Hiển thị danh  sách sản phẩm ra ngoài giao diện quản lý sản phẩm

--- src/controller/admin/product.controller.js
+++ src/controller/admin/product.controller.js
@@ -1,7 +1,15 @@
-module.exports.index = (req, res) => {
+const productModel = require('../../models/product.model');
+
+module.exports.index = async (req, res) => {
+
+    const products = await productModel.find({
+        deleted: false
+    });
+    console.log(products);
     res.render('admin/pages/product/index.pug', {
         pageTitle: 'Quản lý sản phẩm',
         lickReload: '/admin/products',
         activeProduct: true,
+        products: products
     });
 }
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -6,7 +6,20 @@ const productSchema = new mongoose.Schema({
     productCode: String,
     description: String,
     price: Number,
-    discountPercent: Number
+    discountPercent: Number,
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'active'
+    },
+    position: Number,
+    quantity: Number,
+    deleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
 });
 
 const productModel = mongoose.model('products', productSchema);
